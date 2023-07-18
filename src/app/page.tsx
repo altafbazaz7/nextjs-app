6084d81e67974bd27c279766d1da4cfcee56470d
altafbazaz7 <ermohammadaltaf@gmail.com>
2023-07-18 22:18:51 +0530

    first

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,335 @@
-import Image from 'next/image'
+"use client"
 
-export default function Home() {
+import React, { useEffect, useState } from 'react'
+
+const Home = () => {
+  const [ showSearchBox, setShowSearchBox] = useState(false)
+  const [ data , setData ] =useState<object | any>([]); 
+  const [search, setSearch] = useState("");
+  const [ isAscendingOrder, setIsAscendingOrder]= useState<any>(false)
+
+  const fetchData = async() => {
+      const response = await fetch("https://dummyjson.com/products")
+      const actualRes = await response.json()
+      setData(actualRes.products)
+      console.log(data, "actualRes")
+  }
+
+  useEffect(() => {
+    fetchData()
+  },[])
+
+
+
+  // SORT DATA BY NAME
+  const handleSortFunctionTablebyName = () => {
+    setData((prevState: any) => {
+      const sortedData = [...prevState];
+      if (isAscendingOrder) {
+        sortedData.sort((a, b) => a.title.localeCompare(b.title));
+      } else {
+        sortedData.sort((a, b) => b.title.localeCompare(a.title));
+      }
+      setIsAscendingOrder(!isAscendingOrder);
+      return sortedData;
+    });
+  };
+
+  // SORT DATA BY CONTACT
+  const handleSortFunctionTablebyContact = () => {
+    setData((prevState: any) => {
+      const sortedData = [...prevState];
+      if (isAscendingOrder) {
+        sortedData.sort((a, b) => a.email.localeCompare(b.email));
+      } else {
+        sortedData.sort((a, b) => b.email.localeCompare(a.email));
+      }
+      setIsAscendingOrder(!isAscendingOrder);
+      return sortedData;
+    });
+  }
+  
+
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+
+    const searchTerm = e.target.value;
+    setSearch(searchTerm);
+
+    if(searchTerm === ''){
+      setData(data)
+    }else{
+         setData((prevState: any[]) => {
+      return prevState.filter((elem: any) => {
+        const titleMatch = elem.title.toLowerCase().includes(searchTerm.toLowerCase());
+        const descriptionMatch = elem.description.toLowerCase().includes(searchTerm.toLowerCase());
+        return titleMatch || descriptionMatch;
+      });
+    });
+    }
+
+ 
+  };
+
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+              <table className="client-screen-table" >
+          <thead style={{ backgroundColor: "#F5F5F5" }}>
+            <tr
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "flex-start",
+              }}
+            >
+              <th
+                style={{
+                  height: "56px",
+                  display: "flex",
+                  justifyContent: "flex-start",
+                  alignItems: "center",
+                  padding: "0px 6px",
+                }}
+              >
+                {" "}
+                Timestamp
+
+                <div className="search-box-tab-1"  style={{visibility: showSearchBox ? "visible" : "hidden"}}>
+                <div className="search-box-table">
+        <input className="input-field" placeholder="Search..." onChange={handleChange}/>
+    
         </div>
-      </div>
+    
+          </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+                <svg     onClick={() =>  setShowSearchBox(!showSearchBox)}   style={{position:"relative", left:"-2%"}} xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="#1D2759" className="bi bi-search  svg-input-search" viewBox="0 0 16 16">
+             <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+             </svg>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+         
+                <span style={{ display: "flex", flexDirection: "column" }}   onClick={handleSortFunctionTablebyName}>
+                  <svg
+                    width="10"
+                    height="8"
+                    viewBox="0 0 10 8"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M5.40962 0.585166C5.21057 0.300808 4.78943 0.300807 4.59038 0.585166L0.300712 6.71327C0.0687404 7.04466 0.305816 7.5 0.710328 7.5H9.28967C9.69418 7.5 9.93126 7.04466 9.69929 6.71327L5.40962 0.585166Z"
+                      fill="#333333"
+                    />
+                  </svg>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+                  <svg
+                    width="10"
+                    height="8"
+                    viewBox="0 0 10 8"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M5.40962 7.41483C5.21057 7.69919 4.78943 7.69919 4.59038 7.41483L0.300712 1.28673C0.0687404 0.955343 0.305816 0.5 0.710328 0.5H9.28967C9.69418 0.5 9.93126 0.955342 9.69929 1.28673L5.40962 7.41483Z"
+                      fill="#D8D3D3"
+                    />
+                  </svg>
+                </span>
+              </th>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+              <th
+              onClick={handleSortFunctionTablebyName}
+                style={{
+                  height: "56px",
+                  display: "flex",
+                  justifyContent: "space-between",
+                  alignItems: "center",
+                  padding: "0px 6px",
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+                }}
+              >
+
+
+                Purchase Id
+                <span style={{ display: "flex", flexDirection: "column" }}>
+                  <svg
+                    width="10"
+                    height="8"
+                    viewBox="0 0 10 8"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    onClick={handleSortFunctionTablebyContact}
+                  >
+                    <path
+                      d="M5.40962 0.585166C5.21057 0.300808 4.78943 0.300807 4.59038 0.585166L0.300712 6.71327C0.0687404 7.04466 0.305816 7.5 0.710328 7.5H9.28967C9.69418 7.5 9.93126 7.04466 9.69929 6.71327L5.40962 0.585166Z"
+                      fill="#333333"
+                    />
+                  </svg>
+
+                  <svg
+                    width="10"
+                    height="8"
+                    viewBox="0 0 10 8"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M5.40962 7.41483C5.21057 7.69919 4.78943 7.69919 4.59038 7.41483L0.300712 1.28673C0.0687404 0.955343 0.305816 0.5 0.710328 0.5H9.28967C9.69418 0.5 9.93126 0.955342 9.69929 1.28673L5.40962 7.41483Z"
+                      fill="#D8D3D3"
+                    />
+                  </svg>
+                </span>
+              </th>
+              <th
+                style={{
+                  height: "56px",
+                  display: "flex",
+                  justifyContent: "flex-start",
+                  alignItems: "center",
+                  padding: "0px 6px",
+                }}
+              >
+                Mail
+                <span style={{ display: "flex", flexDirection: "column" }}>
+              
+
+                </span>
+              </th>
+              <th
+                style={{
+                  height: "56px",
+                  display: "flex",
+                  justifyContent: "flex-start",
+                  alignItems: "center",
+                  transform:"translateX(-5%)"
+                }}
+              >
+               Name
+                
+              </th>
+              <th
+                style={{
+                  height: "56px",
+                  display: "flex",
+                  justifyContent: "flex-start",
+                  alignItems: "center",
+                  padding: "0px 6px",
+                  transform:"translateX(-5%)"
+
+                }}
+              >
+                        <span style={{ display: "flex", flexDirection: "column" }}>
+                
+
+                </span>
+                Source
+              </th>
+              <th
+                style={{
+                  height: "56px",
+                  display: "flex",
+                  justifyContent: "flex-start",
+                  alignItems: "center",
+                  padding: "0px 6px",
+                  transform:"translateX(-5%)"
+
+                }}
+              >
+                        <span style={{ display: "flex", flexDirection: "column" }}>
+                
+                </span>
+                Status
+              </th>
+              <th
+                style={{
+                  height: "56px",
+                  display: "flex",
+                  justifyContent: "flex-start",
+                  alignItems: "center",
+                  padding: "0px 6px",
+                  transform:"translateX(-5%)"
+
+                }}
+              >
+                Select
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {data?.map((client: any, index: any) => (
+              <tr
+                style={{
+                  height: "69px",
+                  background: "#FFFFFF",
+                  boxShadow: "0px -1px 0px #E0E7ED",
+                }}
+                key={index}
+              >
+                <td
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "flex-start",
+                    paddingLeft: ".5rem"
+                  }}
+                >
+                  <span
+                    style={{
+                      color: "#1D2759",
+                      fontWeight: "400",
+                      fontSize: "12px",
+                    }}
+                  >
+                    {" "}
+               {client.title}
+                  </span>
+                  <span>
+                  {client.description.length > 20
+                ? client.description.slice(0, 20) + "..."
+                : client.description}
+
+                  </span>
+                </td>
+                <td>
+                  <span
+                    style={{
+                      display: "flex",
+                      flexDirection: "column",
+                      alignItems: "flex-start",
+                      justifyContent:"center",
+                      textAlign:"left"
+                    }}
+                  >
+                    <span> {client.category}</span>
+                    <span> {client.discountPercentage} %</span>
+                  </span>
+                </td>
+                <td>{client.brand}</td>
+                <td>$ {client.price}  </td>
+                <td>$ {client.price}  </td>
+                <td>$ {client.price}  </td>
+
+                <span className="last-column-item" 
+                    style={{
+                      transform:"translateX(-230%)"
+                    }}
+                >
+                  <td
+                    className="last-column"
+                    style={{
+                      background:"#F8FFEF",
+                    }}
+                  >
+                   
+                      Open
+                  </td>
+                </span>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+    </>
   )
 }
+
+export default Home
